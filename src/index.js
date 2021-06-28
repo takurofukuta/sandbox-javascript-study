13a18afdb298ea48200ffc3b223c2e8cd2b2885f
福田 拓郎 <76578159+takurofukuta@users.noreply.github.com>
2021-06-28 20:48:33 +0900

    javascriptを用いてtodoアプリを作成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const addTask = () => {
+  // 入力された値を取得し、入力フォームを空にする
+  const inputText = document.getElementById("input-text").value;
+  document.getElementById("input-text").value = "";
+
+  // 未完了タスクの追加処理
+  // liタグ作成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // divタグ作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.appendChild(li);
+
+  // 未完了タスクの追加
+  document.getElementById("uncompleted-task").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => addTask());
